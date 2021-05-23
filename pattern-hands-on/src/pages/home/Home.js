be079d3f69e15f54f59d4afb149523c9413d7c17
roshnidevadiga <roshni.devadiga@gmail.com>
2021-05-23 13:49:54 +0530

    added constraints on the click behviour

--- pattern-hands-on/src/pages/home/Home.js
+++ pattern-hands-on/src/pages/home/Home.js
@@ -2,6 +2,15 @@ import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
+/*
+A simple increment decrement counter
+1. min value should be 0
+2. max value should be 10. After this the onClick on increment would not function and button will be disabled
+*/
+
+const MIN_CLICKS = 0,
+MAX_CLICKS = 10;
+
 const useStyles = makeStyles(theme => ({
     root: {
         "& > *": {
@@ -15,9 +24,18 @@ const Home = () => {
     const [clicks, setClick] = useState(0);
 
     const incrementClicks = () => {
+        //Do not allow more than max clicks
+        if(clicks===MAX_CLICKS) {
+            return;
+        }
+
         setClick(prevClickCount => prevClickCount + 1);
     };
     const decrementClicks = () => {
+        //Do not allow less than min clicks
+        if(clicks===MIN_CLICKS) {
+            return;
+        }
         setClick(prevClickCount => prevClickCount - 1);
     };
 
@@ -25,10 +43,10 @@ const Home = () => {
         <>
             Home
             <div className={classes.root}>
-                <Button variant="contained" color="primary" onClick={incrementClicks}>
+                <Button variant="contained" color="primary" onClick={incrementClicks} disabled={clicks===MAX_CLICKS}>
                     Increment
                 </Button>
-                <Button variant="contained" color="primary" onClick={decrementClicks}>
+                <Button variant="contained" color="primary" onClick={decrementClicks} disabled={clicks===MIN_CLICKS}>
                     Decrement
                 </Button>
                 <div>
