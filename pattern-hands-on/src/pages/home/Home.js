053e73f3efcb42444a3df4b6083169ca30dcb168
roshnidevadiga <roshni.devadiga@gmail.com>
2021-05-23 13:41:47 +0530

    simple increment decrement counter with functional components

--- pattern-hands-on/src/pages/home/Home.js
+++ pattern-hands-on/src/pages/home/Home.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        "& > *": {
+            margin: theme.spacing(1),
+        },
+    },
+}));
 
 const Home = () => {
+    const classes = useStyles();
+    const [clicks, setClick] = useState(0);
+
+    const incrementClicks = () => {
+        setClick(prevClickCount => prevClickCount + 1);
+    };
+    const decrementClicks = () => {
+        setClick(prevClickCount => prevClickCount - 1);
+    };
+
     return (
         <>
-        Home
+            Home
+            <div className={classes.root}>
+                <Button variant="contained" color="primary" onClick={incrementClicks}>
+                    Increment
+                </Button>
+                <Button variant="contained" color="primary" onClick={decrementClicks}>
+                    Decrement
+                </Button>
+                <div>
+                    Clicks: <span>{clicks}</span>
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Home;
